98dc5f9b3cff9cd091622556f0126baab466c0c1
Sebastian Marius <sebastianmarius2100@gmail.com>
2024-10-07 14:25:48 +0300

    new key

--- receipe-list/src/apiConfig.js
+++ receipe-list/src/apiConfig.js
@@ -1,2 +1,2 @@
-export const apiKey = 'AIzaSyCsLLU1rzlcEUeUCLU0PO4NmvJFbqjuZxI';
+export const apiKey = 'AIzaSyAsTzfGhV7Ax3_VHcrKutH0ZdeR7BUE2QE';
 export const pexelKey = 'MtcpRqWJ2t9Yf2wYmzylEkvtu9kt7iPvopp5S4LvweRYzk6SVw6WJYkm';
